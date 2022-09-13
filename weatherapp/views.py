5c6db643f32d74d8284458640206ca64601efe31
marcussteel <resetyourcoin21@gmail.com>
2022-09-13 21:39:35 +0300

    add user city

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,23 +1,60 @@
 import imp
+from pyexpat.errors import messages
 from urllib import response
 from django.shortcuts import render
 from decouple import config
 import requests
 from pprint import pprint
+from django.contrib import messages
+from .models import City
 
 def index(request):
     API_KEY=config('API_KEY')
-    city = "Yozgat"
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    city = "yozgat"
+    u_city = request.POST.get("name")
+
+
+    if u_city:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        print(response.ok)
+        if response.ok:
+            content = response.json()
+            r_city = content["name"]
+            if City.objects.filter(name=r_city):
+                messages.warning(request,"City already exist")
+            else:
+                City.objects.create(name=r_city)
+
+            
+            context = {
+                "city": content["name"],
+                "temp": content["main"]["temp"],
+                "desc": content["weather"][0]["description"],
+                "icon": content["weather"][0]["icon"]
+            }
+            return render(request, 'weatherapp/index.html', context)
+        else:
+            messages.warning(request,"no city")
+
+
+
+
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
     response = requests.get(url)
     content= response.json()
     # pprint(content)
-    pprint(content["name"])
-    pprint(content["main"]["temp"])
-    pprint(content["weather"][0]["description"])
-    pprint(content["weather"][0]["icon"])
-
-    return render(request, 'weatherapp/index.html')
+    # pprint(content["name"])
+    # pprint(content["main"]["temp"])
+    # pprint(content["weather"][0]["description"])
+    # pprint(content["weather"][0]["icon"])
+    context={
+        "city" : content["name"],
+        "temp" : content["main"]["temp"],
+        "desc" : content["weather"][0]["description"],
+        "icon" : content["weather"][0]["icon"]
+    }
+    return render(request, 'weatherapp/index.html',context)
 
 
 {'base': 'stations',
