2feff0133b73a7a9a26db8fba879c0a890722bbc
marcussteel <resetyourcoin21@gmail.com>
2022-09-13 21:57:47 +0300

    time out and messages

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,6 +1,3 @@
-import imp
-from pyexpat.errors import messages
-from urllib import response
 from django.shortcuts import render
 from decouple import config
 import requests
@@ -8,33 +5,44 @@ from pprint import pprint
 from django.contrib import messages
 from .models import City
 
-def index(request):
-    API_KEY=config('API_KEY')
-    city = "yozgat"
-    u_city = request.POST.get("name")
 
+def index(request):
+    API_KEY = config("API_KEY")
+    city = "Eskişehir"
+    u_city = request.POST.get("name")
 
     if u_city:
         url = f"https://api.openweathermap.org/data/2.5/weather?q={u_city}&appid={API_KEY}&units=metric"
         response = requests.get(url)
         print(response.ok)
+
         if response.ok:
             content = response.json()
             r_city = content["name"]
             if City.objects.filter(name=r_city):
-                messages.warning(request,"City already exist")
+                messages.warning(request, "City already exists!")
             else:
                 City.objects.create(name=r_city)
 
-            
-            context = {
-                "city": content["name"],
-                "temp": content["main"]["temp"],
-                "desc": content["weather"][0]["description"],
-                "icon": content["weather"][0]["icon"]
-            }
-            return render(request, 'weatherapp/index.html', context)
         else:
-            messages.warning(request,"no city")
+            messages.warning(request, "There is no city")
 
-  
+    city_data = []
+    cities = City.objects.all()
+    for city in cities:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        content = response.json()
+        data = {
+            "city": content["name"],
+            "temp": content["main"]["temp"],
+            "icon": content["weather"][0]["icon"],
+            "desc": content["weather"][0]["description"]
+        }
+        city_data.append(data)
+
+    context = {
+        "city_data": city_data
+    }
+
+    return render(request, 'weatherapp/index.html', context)
