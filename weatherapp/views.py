e9e952d728968a88b73c7fc5ebd28773585aca21
marcussteel <resetyourcoin21@gmail.com>
2022-09-13 20:34:22 +0300

    first commit

---
+++ weatherapp/views.py
@@ -0,0 +1,14 @@
+import imp
+from urllib import response
+from django.shortcuts import render
+from decouple import config
+import requests
+
+def index(request):
+    API_KEY=config('API_KEY')
+    city = "Yozgat"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+    response = requests.get(url)
+    print(response)
+    return render(request, 'weatherapp/index.html')
+
